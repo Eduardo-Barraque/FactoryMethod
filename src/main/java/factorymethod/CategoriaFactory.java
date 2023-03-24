6dbd5a01b7c826ab55224ab6bb338704f225d66a
Eduardo-Barraque <eduabarraque@gmail.com>
2023-03-24 13:53:37 -0300

    First commit

---
+++ src/main/java/factorymethod/CategoriaFactory.java
@@ -0,0 +1,19 @@
+package factorymethod;
+
+public class CategoriaFactory {
+
+    public static ICategoria obterCategoria(String categoria) {
+        Class classe = null;
+        Object objeto = null;
+        try {
+            classe = Class.forName("factorymethod.Categoria" + categoria);
+            objeto = classe.newInstance();
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Categoria inexistente");
+        }
+        if (!(objeto instanceof ICategoria)) {
+            throw new IllegalArgumentException("Categoria inválida");
+        }
+        return (ICategoria) objeto;
+    }
+}
